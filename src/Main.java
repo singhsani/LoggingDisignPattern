8ac0b2a388c8747ebb39bebf5ab279e7f5bd2ac3
includestdioh <pratapsinghh03@gmail.com>
2025-03-02 14:05:48 +0530

    first

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         LoggerProcessor loggerProcessor=new InfoLogProcessor(new DebugProcessor(new ErrorProcessor(null)));
         loggerProcessor.log(LoggerProcessor.DEBUG,"your process is in dubug");
-        System.out.println("Hello, World!");
+        loggerProcessor.log(LoggerProcessor.INFO,"your process is in Info");
+        loggerProcessor.log(LoggerProcessor.ERROR,"your process is in Error");
+
     }
 }
